b3e4c8354ddb95131b3a06f96311d6d2bbd4ea4c
Rizart Dokollari <r.dokollari@acg.edu>
2014-02-25 22:39:12 +0200

    Fully responsive log in.

--- src/me/dokollari/cmanager/swings/MainFrame.java
+++ src/me/dokollari/cmanager/swings/MainFrame.java
@@ -7,23 +7,45 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.TitledBorder;
 
+/**
+ * 
+ * @author Riz
+ * 
+ */
 public class MainFrame
 {
 
+	private static final String LOG_IN_MESSAGE_JLABEL = "Log In";
+	private static final String USER_NAME_MESSAGE = "Username";
+	private static final String PASSWORD_MESSAGE = "Password";
 	private JFrame frame;
 	private JTextField txtUsername;
-	private JTextField txtPassword;
+	private JPasswordField passwordField;
+	private DB db;
 
 	/**
 	 * Launch the application.
@@ -34,35 +56,28 @@ public class MainFrame
 			public void run() {
 				try {
 					MainFrame window = new MainFrame();
-
-					Dimension screenSize = Toolkit.getDefaultToolkit()
-							.getScreenSize();
-					JFrame frame = window.frame;
-					window.frame.setBounds(100, 100, 708, 415);
-					// frame location
-					frame.setLocation(screenSize.width / 2 - frame.getSize().width
-							/ 2, screenSize.height / 2 - frame.getSize().height / 2);
-					frame.setVisible(true);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
-				}
+				} // end catch
 			}
 		});
-	}
+	} // end main method
 
 	/**
 	 * Create the application.
 	 */
 	public MainFrame() {
-		initialize();
-	}
+		initialize(); // initialize UI
+		db = new DB(); // initialize database.
+	} // end constructor
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 708, 415);
+		frame.setBounds(100, 100, 743, 461);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 
@@ -71,65 +86,149 @@ public class MainFrame
 		frame.setLocation(screenSize.width / 2 - frame.getSize().width / 2,
 				screenSize.height / 2 - frame.getSize().height / 2);
 
-		final JPanel panelBackground = new JPanel();
-		panelBackground.setForeground(new Color(51, 51, 51));
-		panelBackground.addComponentListener(new ComponentAdapter()
+		final JPanel panelBackgroundBack = new JPanel();
+		panelBackgroundBack.setForeground(new Color(51, 51, 51));
+		panelBackgroundBack.addComponentListener(new ComponentAdapter()
 		{
 			@Override
 			public void componentResized(ComponentEvent arg0) {
-				int lastComponent = panelBackground.getComponentCount() - 1;
-				int panelWidth = panelBackground.getWidth();
-				int panelHeight = panelBackground.getHeight();
+				Component firstComponent = panelBackgroundBack.getComponent(0);
+				Component lastComponent = panelBackgroundBack
+						.getComponent(panelBackgroundBack.getComponentCount() - 1);
+				int panelWidth = panelBackgroundBack.getWidth();
+				int panelHeight = panelBackgroundBack.getHeight();
 
-				panelBackground.getComponent(lastComponent).setSize(panelWidth,
-						panelHeight);
-
-				for (int i = 0; i < panelBackground.getComponentCount() - 1; i++) {
-					Component component = panelBackground.getComponent(i);
-					component.setLocation((int) (panelBackground.getWidth() / 2 - component.getSize().getWidth()/2),
-							component.getY());
-				}
-
-			}
+				lastComponent.setSize(panelWidth, panelHeight);
+				firstComponent.setLocation(
+						(int) (panelBackgroundBack.getWidth() / 2 - firstComponent
+								.getSize().getWidth() / 2),
+						(int) (panelBackgroundBack.getHeight() / 2
+								- firstComponent.getSize().getHeight() / 2));
+				// for (int i = 0; i < panelBackground.getComponentCount() - 1; i++)
+				// {
+				// Component component = panelBackground.getComponent(i);
+				// component.setLocation(
+				// (int) (panelBackground.getWidth() / 2 - component
+				// .getSize().getWidth() / 2), component.getY());
+				// } // end for
+			} // end method componentResized
 		});
-		frame.getContentPane().add(panelBackground);
+		frame.getContentPane().add(panelBackgroundBack);
+		panelBackgroundBack.setLayout(null);
 
-		JButton btnLogIn = new JButton("Sign In");
-		btnLogIn.setIcon(new ImageIcon(MainFrame.class
-				.getResource("/img/button_login.png")));
-		btnLogIn.setBounds(252, 239, 250, 40);
+		JPanel panelBackgroundFront = new JPanel();
+		panelBackgroundFront.setBounds(222, 62, 314, 277);
+		panelBackgroundBack.add(panelBackgroundFront);
 
-		txtPassword = new JTextField();
-		txtPassword.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		txtPassword.setForeground(Color.LIGHT_GRAY);
-		txtPassword.setBounds(252, 182, 250, 35);
-		txtPassword.setText("Password");
-		txtPassword.setColumns(10);
+		JLabel lblBackgroundFront = new JLabel("Front Background");
+		lblBackgroundFront.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBackgroundFront.setBounds(0, 0, 314, 277);
+		lblBackgroundFront.setIcon(new ImageIcon(MainFrame.class
+				.getResource("/img/background_small.png")));
 
 		txtUsername = new JTextField();
-		txtUsername.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		txtUsername.setForeground(Color.LIGHT_GRAY);
-		txtUsername.setBounds(252, 128, 250, 35);
-		txtUsername.setText("Username");
+		txtUsername.setBounds(34, 72, 250, 35);
+		txtUsername.setBorder(new MatteBorder(0, 5, 0, 0, (Color) null));
+		txtUsername.setCaretColor(new Color(0, 153, 255));
+		txtUsername.setBackground(new Color(255, 255, 255));
+		txtUsername.addFocusListener(new FocusAdapter()
+		{
+			@Override
+			public void focusGained(FocusEvent e) {
+				String userName = txtUsername.getText();
+
+				if (userName.equals(USER_NAME_MESSAGE)) {
+					txtUsername.setBorder(new MatteBorder(0, 5, 0, 0,
+							(Color) new Color(102, 204, 255)));
+					txtUsername.setFont(new Font("Rockwell", Font.PLAIN, 13));
+					txtUsername.setForeground(new Color(51, 51, 51));
+					txtUsername.setText("");
+				} // end if
+			} // end method focusGainec
+
+			@Override
+			public void focusLost(FocusEvent arg0) {
+				String userName = txtUsername.getText();
+
+				if (userName.equals("")) {
+					txtUsername.setBorder(new LineBorder(null));
+					txtUsername.setText(USER_NAME_MESSAGE);
+					txtUsername.setForeground(new Color(204, 204, 204));
+					txtUsername.setFont(new Font("Rockwell", Font.ITALIC, 13));
+				} // end if
+			} // end method focusLost
+		});
+		txtUsername.setFont(new Font("Open Sans", Font.ITALIC, 13));
+		txtUsername.setForeground(new Color(204, 204, 204));
+		txtUsername.setText(USER_NAME_MESSAGE);
 		txtUsername.setColumns(10);
 
+		final JLabel lblLogin = new JLabel(LOG_IN_MESSAGE_JLABEL);
+		lblLogin.setBounds(34, 47, 250, 14);
+		lblLogin.setFont(new Font("Open Sans", Font.PLAIN, 11));
+		lblLogin.setForeground(new Color(51, 51, 51));
+
+		passwordField = new JPasswordField();
+		passwordField.setBounds(34, 123, 250, 35);
+		passwordField.setCaretColor(new Color(0, 153, 255));
+		passwordField.setToolTipText("Your password.");
+		passwordField.setText(PASSWORD_MESSAGE);
+		passwordField.addFocusListener(new FocusAdapter()
+		{
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (new String(passwordField.getPassword())
+						.equals(PASSWORD_MESSAGE)) {
+					passwordField.setBorder(new MatteBorder(0, 5, 0, 0,
+							(Color) new Color(102, 204, 255)));
+					passwordField.setText("");
+				} // end if
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (new String(passwordField.getPassword()).equals("")) {
+					passwordField.setText(PASSWORD_MESSAGE);
+				} // end if
+			}
+		});
+		passwordField.setFont(new Font("Open Sans", Font.BOLD, 13));
+
+		final JButton btnLogIn = new JButton("Sign In");
+		btnLogIn.setBounds(34, 183, 250, 40);
+		btnLogIn.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0) {
+				String userName = txtUsername.getText();
+				db.userExists(userName, passwordField.getPassword());
+				System.out.println("User exists");
+			} // end method actionPerformed
+		});
+		btnLogIn.setIcon(new ImageIcon(MainFrame.class
+				.getResource("/img/button_login.png")));
+		panelBackgroundFront.setLayout(null);
+		panelBackgroundFront.add(txtUsername);
+		panelBackgroundFront.add(lblLogin);
+		panelBackgroundFront.add(passwordField);
+		panelBackgroundFront.add(btnLogIn);
+		panelBackgroundFront.add(lblBackgroundFront);
+
 		JLabel lblBackground = new JLabel("background");
-		lblBackground.setBounds(0, 0, 700, 391);
+		lblBackground.setBorder(UIManager.getBorder("FormattedTextField.border"));
+		lblBackground.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		lblBackground.setForeground(new Color(102, 102, 102));
+		lblBackground.setBounds(0, 0, 735, 437);
 		lblBackground.setHorizontalAlignment(SwingConstants.CENTER);
 		lblBackground.setIcon(new ImageIcon(MainFrame.class
 				.getResource("/img/background.jpg")));
-		panelBackground.setLayout(null);
-		panelBackground.add(txtUsername);
-		panelBackground.add(txtPassword);
-		panelBackground.add(btnLogIn);
+		panelBackgroundBack.add(lblBackground);
 
-		final JLabel lblLogin = new JLabel("Log In");
-		lblLogin.setForeground(new Color(51, 51, 51));
-		lblLogin.setBounds(252, 103, 250, 14);
-		panelBackground.add(lblLogin);
-		panelBackground.add(lblBackground);
-
-		frame.pack();
-		btnLogIn.requestFocusInWindow();
+		// solves the problem of btnLogIn not getting focus
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run() {
+				btnLogIn.requestFocusInWindow();
+			}
+		});
 	}
 }
